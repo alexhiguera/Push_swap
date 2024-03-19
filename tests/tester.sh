2853d4fa0799f879a86534f954e4f27fed845675
Columbux <ahiguera@student.42urduliz.com>
2024-03-19 17:23:13 +0100

    feature: save logs of tester

--- tests/tester.sh
+++ tests/tester.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+time=$(date "+%H:%M - %d/%m/%Y")
+echo "\n👾 Push_swap Testing logs:" $time "\n" >> logs.txt
 if [ $# -eq 0 ]
 then 
 	read -p "Total de pruebas a realizar: " TOTAL
@@ -15,22 +17,21 @@ else
 	echo "Demasiados argumentos"
 	exit
 fi
-
 var=0;
 echo "realizando pruebas..."
 
 for i in $(seq $TOTAL)
 do
 	ARG=$(seq -$lenlist $lenlist | sort -R | head -n $lenlist | tr '\n' ' ')
-	#echo $ARG
+	echo $ARG | >> logs.txt
 	ret=$(./push_swap $ARG | wc -l)
 	ret2=$(./push_swap $ARG | ./tests/checker_Mac $ARG )
 	var=$(($ret + $var))
-	echo "$ret -----> $ret2"
+	echo "$ret -----> $ret2" | tee -a logs.txt
 	if [ $(($i % 10)) -eq 0 ]
 	then
-		echo "..."
+		echo "..." | tee -a logs.txt
 	fi
 done
 
-echo Media de pasos necesarios: $(($var / $TOTAL))! 🚀
+echo Media de pasos necesarios: $(($var / $TOTAL))! 🚀 | tee -a logs.txt
